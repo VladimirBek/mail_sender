f2941da651ad4819d43a4691c8fc187113d9190d
VladimirBek <vladimirbek1996@gmail.com>
2023-08-23 23:59:40 +0300

    added check_mailing_ready func

--- mail_sender/services.py
+++ mail_sender/services.py
@@ -31,9 +31,34 @@ def execute_mailing(mailing_list: MailingList):
             server_report = 'неизвестная ошибка'
         finally:
             MailingLog.objects.create(
-                last_send=datetime.now(),
+                last_send=datetime.now(timezone.utc),
                 status=status,
                 server_report=server_report,
                 clients=mailing_list.clients.all(),
                 mailing_list=mailing_list
             )
+
+
+def check_mailing_ready():
+    active_mailing = MailingList.objects.filter(status='запущена')
+    for mailing_list in active_mailing:
+        mailing_time = mailing_list.settings.time
+        if mailing_time <= datetime.now().time():
+            last_log = MailingLog.objects.filter(mailing_list=mailing_list).order_by('pk').last()
+            if last_log:
+                last_send = last_log.last_send
+                time_now = datetime.now(timezone.utc)
+                frequency = mailing_list.settings.frequency
+                diff = time_now - last_send
+                if frequency == 'ежедневно' and diff >= 1:
+                    execute_mailing(mailing_list)
+                elif frequency == 'еженедельно' and diff >= 7:
+                    execute_mailing(mailing_list)
+                elif frequency == 'ежемесячно' and diff >= 30:
+                    execute_mailing(mailing_list)
+            else:
+                pass
+
+
+
+
