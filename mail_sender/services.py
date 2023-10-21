c6c51893084588f32e9f79f2fc20c54e8b5bd01b
VladimirBek <vladimirbek1996@gmail.com>
2023-10-21 18:31:33 +0300

    added mail_send_script and cron task

--- mail_sender/services.py
+++ mail_sender/services.py
@@ -37,6 +37,25 @@ def execute_mailing(mailing_list: MailingList):
             instance.clients.set(mailing_list.clients.all())
 
 
+def check_mailing_ready():
+    active_mailing = MailingList.objects.filter(status='запущена')
+    for mailing_list in active_mailing:
+        mailing_time = mailing_list.settings.time
+        if mailing_time <= datetime.now().time():
+            last_log = MailingLog.objects.filter(mailing_list=mailing_list).order_by('pk').last()
+            if last_log:
+                last_send = last_log.last_send
+                time_now = datetime.now(timezone.utc)
+                frequency = mailing_list.settings.frequency
+                diff = time_now - last_send
+                if frequency == 'ежедневно' and diff >= 1:
+                    execute_mailing(mailing_list)
+                elif frequency == 'еженедельно' and diff >= 7:
+                    execute_mailing(mailing_list)
+                elif frequency == 'ежемесячно' and diff >= 30:
+                    execute_mailing(mailing_list)
+            else:
+                pass
 
 
 
